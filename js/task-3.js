3f7f6432fef78519a431864933b179caa4984152
StarovDrii <131916457+StarovDrii@users.noreply.github.com>
2023-10-24 00:49:03 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -19,9 +19,9 @@ function getElementWidth(content, padding, border) {
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
 
 // Залиш цей код для перевірки ментором.
 
